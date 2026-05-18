f45ffe97e4bfafc496a3b401b6894061495dc3b2
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-05-18 19:59:28 +0300

    Added more difficulty levels to List2
    Expanded List2 to include additional difficulty levels.

--- numbers.js
+++ numbers.js
@@ -1,6 +1,8 @@
 document.body.innerHTML += 'N represents a positive integer';
 const List = [["1×1×1 Cube (already solved)", "Any 1×1×N cuboid (trivial solution)"]];
-const List2 = ["Effortless", "Very Easy", "Easy", "Medium", "Hard", "Very Hard", "Challenging"]
+const List2 = ["Effortless", "Very Easy", "Easy", "Medium", "Hard", "Very Hard", "Challenging", "Intense", "Insane", 
+"Extreme", "Extreme+", "Nightmare", "Nightmare+", "Hell", "Hell+", "Easy Demon", "Medium Demon", "Hard Demon", "Very Hard Demon", "Challenging Demon",
+"Intense Demon", "Insane Demon", "Extrem Demon"]
 var x = [0, 0];
 var y = 0;
 for(x[1] = 0; x[1] < List.length; x++) {
