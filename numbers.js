6a983bcabc6914928555b4dc1fd8f4413e7aed9b
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-05-19 17:54:25 +0300

    Updated HTML content and added new cuboid types

--- numbers.js
+++ numbers.js
@@ -1,12 +1,13 @@
-document.body.innerHTML += 'N represents a positive integer';
-const List = [["1×1×1 Cube (already solved)", "Any 1×1×N cuboid (trivial solution)"]];
+document.body.innerHTML += '<p id="N">N represents a positive integer</p>';
+const List = [["1×1×1 Cube (already solved)", "Any 1×1×N cuboid (trivial solution)", "1×2×2 Cuboid", "Square-(-1) (identical to 1×2×3)", "1×2×3 Cuboid"]];
 const List2 = ["Effortless", "Very Easy", "Easy", "Medium", "Hard", "Very Hard", "Challenging", "Intense", "Insane", 
 "Extreme", "Extreme+", "Nightmare", "Nightmare+", "Hell", "Hell+", "Easy Demon", "Medium Demon", "Hard Demon", "Very Hard Demon", "Challenging Demon",
-"Intense Demon", "Insane Demon", "Extrem Demon"]
+"Intense Demon", "Insane Demon", "Extreme Demon"]
 var x = [0, 0];
 var y = 0;
 for(x[1] = 0; x[1] < List.length; x++) {
   for(x[0] = 0; x[1] < List[x[1]].length; x++) {
    document.body.innerHTML += '<p id=`Puzzle${y}`>'+(x[0]+1).toString()+'. '+List[x[1]][x[0]].toString()+'</p>';
+   y++;
 }
 }
