f982609a4d7774ddd455bc7e50e32545b1bf6c35
Githubber2662 <95174200+Githubber2662@users.noreply.github.com>
2026-05-19 17:57:21 +0300

    Fixed nested loop condition in numbers.js

--- numbers.js
+++ numbers.js
@@ -6,7 +6,7 @@ const List2 = ["Effortless", "Very Easy", "Easy", "Medium", "Hard", "Very Hard",
 var x = [0, 0];
 var y = 0;
 for(x[1] = 0; x[1] < List.length; x++) {
-  for(x[0] = 0; x[1] < List[x[1]].length; x++) {
+  for(x[0] = 0; x[0] < List[x[1]].length; x++) {
    document.body.innerHTML += '<p id=`Puzzle${y}`>'+(x[0]+1).toString()+'. '+List[x[1]][x[0]].toString()+'</p>';
    y++;
 }
